a6ab16d06d2cfc2cb3e274d29238a3914f01703b
brian_pos <brian_pos@hotmail.com>
2022-06-23 16:21:33 +1000

    Include the missing interface

--- types/vue2-brace-editor.d.ts
+++ types/vue2-brace-editor.d.ts
@@ -1,12 +1,26 @@
 declare module "vue2-brace-editor"
 
-declare interface TextHighlightRule {
-    start: TextHighlightToken[];
-    charTypes?: TextHighlightToken[];
-    quantifiers?: TextHighlightToken[];
-    charclass?: TextHighlightToken[];
-    groups?: TextHighlightToken[];
-    xGroup?: TextHighlightToken[];
-    comment?: TextHighlightToken[];
-    no_regex?: TextHighlightToken[];
+// declare interface TextHighlighter {
+//     $id: string;
+//     $rules: any;
+// }
+
+declare interface TextHighlightToken {
+    token?: string | string[];
+    regex?: RegExp | string;
+    next?: string;
+    consumeLineEnd?: boolean;
+    defaultToken?: string;
+    push?: string | string[];
 }
+
+// declare interface TextHighlightRule {
+//     start: TextHighlightToken[];
+//     charTypes?: TextHighlightToken[];
+//     quantifiers?: TextHighlightToken[];
+//     charclass?: TextHighlightToken[];
+//     groups?: TextHighlightToken[];
+//     xGroup?: TextHighlightToken[];
+//     comment?: TextHighlightToken[];
+//     no_regex?: TextHighlightToken[];
+// }
