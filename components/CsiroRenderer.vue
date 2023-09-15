0c42691041156fdee208a8be2d4d1c778ef85852
Sean Fong <sean.fong@csiro.au>
2023-09-15 10:44:02 -0700

    Test integration with CSIRO smart forms renderer

---
+++ components/CsiroRenderer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Renderer :questionnaire="questionnaire"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import {applyReactInVue} from "vuereact-combined";
+import {SmartFormsRenderer} from "@aehrc/smart-forms-renderer";
+import QTest from "~/components/TestQues.json";
+
+export default Vue.extend({
+  data() {
+    return {
+      questionnaire: QTest,
+    }
+  },
+  components: {
+    Renderer: applyReactInVue(SmartFormsRenderer)
+  },
+});
+</script>
