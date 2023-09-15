0c149ee69d97850c760031e4bcb0172e731f9941
Sean Fong <sean.fong@csiro.au>
2023-09-15 11:17:16 -0700

    Add button to log response to console

--- components/Questionnaire/EditorRendererSection.vue
+++ components/Questionnaire/EditorRendererSection.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <template v-if="questionnaire">
-      <Renderer :questionnaire="questionnaire" />
+      <Renderer :questionnaire="questionnaire"/>
+      <v-btn color="primary" @click="logResponse()">Log response</v-btn>
+<!--      <div>-->
+<!--        <pre>{{ JSON.stringify(response, null, 2) }}</pre>-->
+<!--      </div>-->
     </template>
     <template v-else>
       <p>No questionnaire provided</p>
@@ -9,13 +13,32 @@
   </div>
 </template>
 
+<style scoped>
+pre {
+  font-size: 0.75em;
+}
+
+</style>
+
 <script lang="ts">
 import Vue, {PropType} from "vue";
 import {applyReactInVue} from "vuereact-combined";
-import {SmartFormsRenderer} from "@aehrc/smart-forms-renderer";
-import {Questionnaire} from "fhir/r4b";
+import {SmartFormsRenderer, getResponse} from "@aehrc/smart-forms-renderer";
+import {Questionnaire, QuestionnaireResponse} from "fhir/r4";
 
 export default Vue.extend({
+  // data() {
+  //   return {
+  //     response: null as QuestionnaireResponse | null,
+  //   };
+  // },
+  methods: {
+    logResponse() {
+      const response = getResponse();
+      // this.response = response;
+      console.log(response)
+    }
+  },
   props: {
     questionnaire: Object as PropType<Questionnaire>
   },
