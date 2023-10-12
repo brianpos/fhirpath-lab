63cc5467390183fefdbb226c357b74629a9c0bb0
Brian Postlethwaite <brian_pos@hotmail.com>
2023-10-12 11:49:24 +1100

    Add the links to all the normative spec functions

--- components/ParseTreeTab.vue
+++ components/ParseTreeTab.vue
@@ -119,6 +119,122 @@ import fhirpath from "fhirpath";
 import fhirpath_r4_model from "fhirpath/fhir-context/r4";
 import fhirpath_r5_model from "fhirpath/fhir-context/r5";
 
+const specFunctionReferences = {
+  // Section 5 Functions
+  // Section 5.1: Existence
+  "empty": "http://hl7.org/fhirpath/N1/#empty-boolean", // 5.1.1
+  "exists": "http://hl7.org/fhirpath/N1/#existscriteria-expression-boolean", // 5.1.2
+  "all": "http://hl7.org/fhirpath/N1/#allcriteria-expression-boolean", // 5.1.3
+  "allTrue": "http://hl7.org/fhirpath/N1/#alltrue-boolean", // 5.1.4
+  "anyTrue": "http://hl7.org/fhirpath/N1/#anytrue-boolean", // 5.1.5
+  "allFalse": "http://hl7.org/fhirpath/N1/#allfalse-boolean", // 5.1.6
+  "anyFalse": "http://hl7.org/fhirpath/N1/#anyfalse-boolean", // 5.1.7
+  "subsetOf": "http://hl7.org/fhirpath/N1/#subsetofother-collection-boolean", // 5.1.8
+  "supersetOf": "http://hl7.org/fhirpath/N1/#supersetofother-collection-boolean", // 5.1.9
+  "count": "http://hl7.org/fhirpath/N1/#count-integer", // 5.1.10
+  "distinct": "http://hl7.org/fhirpath/N1/#distinct-collection", // 5.1.11
+  "isDistinct": "http://hl7.org/fhirpath/N1/#isdistinct-boolean", // 5.1.12
+
+  // Section 5.2: Filtering and projection
+  "where": "http://hl7.org/fhirpath/N1/#wherecriteria-expression-collection", // 5.2.1
+  "select": "http://hl7.org/fhirpath/N1/#selectprojection-expression-collection", // 5.2.2
+  "repeat": "http://hl7.org/fhirpath/N1/#repeatprojection-expression-collection", // 5.2.3
+  "ofType": "http://hl7.org/fhirpath/N1/#oftypetype-type-specifier-collection", // 5.2.4
+
+  // Section 5.3: Subsetting
+  // "": "", // 5.3.1 The `[]` indexer isn't being hyperlinked in the fhirpath lab
+  "single": "http://hl7.org/fhirpath/N1/#single-collection", // 5.3.2
+  "first": "http://hl7.org/fhirpath/N1/#first-collection", // 5.3.3
+  "last": "http://hl7.org/fhirpath/N1/#last-collection", // 5.3.4
+  "tail": "http://hl7.org/fhirpath/N1/#tail-collection", // 5.3.5
+  "skip": "http://hl7.org/fhirpath/N1/#skipnum-integer-collection", // 5.3.6
+  "take": "http://hl7.org/fhirpath/N1/#takenum-integer-collection", // 5.3.7
+  "intersect": "http://hl7.org/fhirpath/N1/#intersectother-collection-collection", // 5.3.8
+  "exclude": "http://hl7.org/fhirpath/N1/#excludeother-collection-collection", // 5.3.9
+
+  // Section 5.4: Combining
+  "union": "http://hl7.org/fhirpath/N1/#unionother-collection", // 5.4.1
+  "combine": "http://hl7.org/fhirpath/N1/#combineother-collection-collection", // 5.4.2
+
+  // Section 5.5: Conversion
+  "iif": "http://hl7.org/fhirpath/N1/#iifcriterion-expression-true-result-collection-otherwise-result-collection-collection", // 1
+  "toBoolean": "http://hl7.org/fhirpath/N1/#toboolean-boolean", // 2a
+  "convertsToBoolean": "http://hl7.org/fhirpath/N1/#convertstoboolean-boolean", // 2b
+  "toInteger": "http://hl7.org/fhirpath/N1/#tointeger-integer", // 3a
+  "convertsToInteger": "http://hl7.org/fhirpath/N1/#convertstointeger-boolean", // 3b
+  "toDate": "http://hl7.org/fhirpath/N1/#todate-date", // 4a
+  "convertsToDate": "http://hl7.org/fhirpath/N1/#convertstodate-boolean", // 4b
+  "toDateTime": "http://hl7.org/fhirpath/N1/#todatetime-datetime", // 5a
+  "convertsToDateTime": "http://hl7.org/fhirpath/N1/#convertstodatetime-boolean", // 5b
+  "toDecimal": "http://hl7.org/fhirpath/N1/#todecimal-decimal", // 6a
+  "convertsToDecimal": "http://hl7.org/fhirpath/N1/#convertstodecimal-boolean", // 6b
+  "toQuantity": "http://hl7.org/fhirpath/N1/#toquantityunit-string-quantity", // 7a
+  "convertsToQuantity": "http://hl7.org/fhirpath/N1/#convertstoquantityunit-string-boolean", // 7b
+  "toString": "http://hl7.org/fhirpath/N1/#tostring-string", // 8a
+  "convertsToString": "http://hl7.org/fhirpath/N1/#convertstostring-string", // 8b
+  "toTime": "http://hl7.org/fhirpath/N1/#totime-time", // 9a
+  "convertsToTime": "http://hl7.org/fhirpath/N1/#convertstotime-boolean", // 9b
+
+  // Section 5.6: String Manipulation
+  "indexOf": "http://hl7.org/fhirpath/N1/#indexofsubstring-string-integer", // 5.6.1
+  "substring": "http://hl7.org/fhirpath/N1/#substringstart-integer-length-integer-string", // 5.6.2
+  "startsWith": "http://hl7.org/fhirpath/N1/#startswithprefix-string-boolean", // 5.6.3
+  "endsWith": "http://hl7.org/fhirpath/N1/#endswithsuffix-string-boolean", // 5.6.4
+  "contains": "http://hl7.org/fhirpath/N1/#containssubstring-string-boolean", // 5.6.5
+  "upper": "http://hl7.org/fhirpath/N1/#upper-string", // 5.6.6
+  "lower": "http://hl7.org/fhirpath/N1/#lower-string", // 5.6.7
+  "replace": "http://hl7.org/fhirpath/N1/#replacepattern-string-substitution-string-string", // 5.6.8
+  "matches": "http://hl7.org/fhirpath/N1/#matchesregex-string-boolean", // 5.6.9
+  "replaceMatches": "http://hl7.org/fhirpath/N1/#replacematchesregex-string-substitution-string-string", // 5.6.10
+  "length": "http://hl7.org/fhirpath/N1/#length-integer", // 5.6.11
+  "toChars": "http://hl7.org/fhirpath/N1/#tochars-collection", // 5.6.12
+
+  // Section 5.7: Math
+  "abs": "http://hl7.org/fhirpath/N1/#abs-integer-decimal-quantity", // 5.7.1
+  "ceiling": "http://hl7.org/fhirpath/N1/#ceiling-integer", // 5.7.2
+  "exp": "http://hl7.org/fhirpath/N1/#exp-decimal", // 5.7.3
+  "floor": "http://hl7.org/fhirpath/N1/#floor-integer", // 5.7.4
+  "ln": "http://hl7.org/fhirpath/N1/#ln-decimal", // 5.7.5
+  "log": "http://hl7.org/fhirpath/N1/#logbase-decimal-decimal", // 5.7.6
+  "power": "http://hl7.org/fhirpath/N1/#powerexponent-integer-decimal-integer-decimal", // 5.7.7
+  "round": "http://hl7.org/fhirpath/N1/#roundprecision-integer-decimal", // 5.7.8
+  "sqrt": "http://hl7.org/fhirpath/N1/#sqrt-decimal", // 5.7.9
+  "truncate": "http://hl7.org/fhirpath/N1/#truncate-integer", // 5.7.10
+
+  // Section 5.8: Tree Navigation
+  "children": "http://hl7.org/fhirpath/N1/#children-collection", // 5.8.1
+  "descendants": "http://hl7.org/fhirpath/N1/#descendants-collection", // 5.8.2
+
+  // Section 5.9: Utility Functions
+  "trace": "http://hl7.org/fhirpath/N1/#tracename-string-projection-expression-collection", // 5.9.1
+  "now": "http://hl7.org/fhirpath/N1/#now-datetime", // 5.9.2a
+  "timeOfDay": "http://hl7.org/fhirpath/N1/#timeofday-time", // 5.9.2b
+  "today": "http://hl7.org/fhirpath/N1/#today-date", // 5.9.2c
+
+  // Section 6.3: Types
+  // 6.3.1 the `is` type specifier is not hyperlinked in the fhirpath lab
+  "is": "http://hl7.org/fhirpath/N1/#istype-type-specifier", // 6.3.2
+  // 6.3.3 the `as` type specifier is not hyperlinked in the fhirpath lab
+  "as": "http://hl7.org/fhirpath/N1/#as-type-specifier", // 6.3.4
+
+  // Section 6.4: Collections
+  // 6.4.1 The `|` operator isn't hyperlinked in the fhirpath lab
+  "in": "http://hl7.org/fhirpath/N1/#in-membership", // 6.4.2
+  // 6.4.3 The `contains` operator isn't hyperlinked in the fhirpath lab
+
+  // Section 6.5: Boolean Logic
+  "and": "http://hl7.org/fhirpath/N1/#and", // 6.5.1
+  "or": "http://hl7.org/fhirpath/N1/#or", // 6.5.2
+  "not": "http://hl7.org/fhirpath/N1/#not-boolean", // 6.5.3
+  "xor": "http://hl7.org/fhirpath/N1/#xor", // 6.5.4
+  "implies": "http://hl7.org/fhirpath/N1/#implies", // 6.5.5
+
+  // Section 6.6: Math operators are not hyperlinked in the fhirpath lab
+
+  // Section 7: Aggregates
+  "aggregate": "http://hl7.org/fhirpath/N1/#aggregateaggregator-expression-init-value-value", // 7.1
+};
+
 // Workaround to include the definition of the parse function
 // (which is actually there, just not in the definition)
 declare module "fhirpath" {
