a99288052492b2b704ce627a6f846411d02906b0
Lukas Schmierer <lukas.schmierer@damedic.ai>
2025-05-20 18:23:59 +0200

    Integrate Go wasm-exec.js as Nuxt compiler plugin

---
+++ plugins/go-wasm-exec.js
@@ -0,0 +1,4 @@
+// Import the wasm_exec.js file to make it part of the build
+import '~/fhir-toolbox-go/wasm_exec.js';
+
+// This plugin doesn't need to do anything else, as wasm_exec.js sets up the Go object globally
