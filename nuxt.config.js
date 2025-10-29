126f53f3558c630297780cf91ee60f61fa0ecab7
Sean Fong <sean.fong@csiro.au>
2025-10-29 11:15:34 +1030

    Upgrade CSIRO renderer to v1.2.8

--- nuxt.config.js
+++ nuxt.config.js
@@ -78,11 +78,12 @@ export default {
       "openai",
       "marked",
       "antlr4",
+      "sql-on-fhir-v2",
       /* Below are dependencies of CSIRO renderer */
       "@mui",
       "@emotion",
       "@tanstack",
-      "sql-on-fhir-v2",
+      "uuid",
     ],
   }
 }
